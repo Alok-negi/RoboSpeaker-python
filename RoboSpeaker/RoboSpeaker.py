d1374f62d85a553e95f0315156ff71f986c23f29
Alok-negi <123154588+Alok-negi@users.noreply.github.com>
2024-01-20 19:59:48 +0530

    Add files via upload

---
+++ RoboSpeaker/RoboSpeaker.py
@@ -0,0 +1,18 @@
+import os
+
+if __name__ == '__main__':
+
+    print("Welcome to RoboSpeaker ")
+
+    while True:
+
+        x = input("Enter what you want me to speak: ")
+
+        if x == "q":
+
+            os.system("say 'bye bye'")
+
+            break
+
+        command = f"say {x}"
+        os.system(command)
